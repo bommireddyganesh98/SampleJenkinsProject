6bacec5edc4b5f74008467df37e0fd45c4be647f
bommi <bommi@Ganesh>
2024-05-21 10:48:17 +0530

    latestcommit

--- SampleJenkinsProject/src/main/java/com/base/BaseClass.java
+++ SampleJenkinsProject/src/main/java/com/base/BaseClass.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -32,6 +33,11 @@ public class BaseClass {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 			System.out.println("Chrome WebDriver initialized.");
+		}
+		else if(browser.equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+			System.out.println("Firefox WebDriver Initialized");
 		}
 
 		driver.get(url);
